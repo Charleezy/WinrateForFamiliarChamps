3d7ecc03fbdc4178e57661206ce2b3716527554b
craig-sh <craig.henriques@mail.utoronto.ca>
2015-04-15 21:20:11 -0400

    Added data from runs on local machine...around 280k games

--- api_wrapper/FILES.py
+++ api_wrapper/FILES.py
@@ -3,6 +3,7 @@
 #       GAME_FILE = '/tmp/processed_games.pickle'
 #       PROSCESSED_SUMM_FILE = '/tmp/processed_summ.pickle'
 #       SQLLITE_FILE = '/tmp/testdb.db'
+#       Prepopulated files can be found in ../crawler_raw_data/
 
 SUMMONER_FILE =
 GAME_FILE =
